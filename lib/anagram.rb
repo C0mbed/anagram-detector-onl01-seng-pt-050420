48ce728f865ec98c8dbda9a76fe71ddf587fe51e
Matt Farncombe <matt@farncombe.co>
2020-05-26 11:50:50 +0000

    Automatically backed up by Learn

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -11,9 +11,10 @@ class Anagram
     match_array.each do |entry|
       entry_sorted = entry.split("")
       if word_sorted.sort == entry_sorted.sort
-        return @word
+        @match_array << @word
       end
     end
+    return @match_array
   end
 
 end
