2a4d1f9f0bf5d250d177f0c130b749ce7d00df6e
Matt Farncombe <matt@farncombe.co>
2020-05-26 11:48:19 +0000

    Automatically backed up by Learn

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,1 +1,18 @@
 # Your code goes here!
+
+class Anagram
+  def initialize(word)
+    @word = word
+    @match_array = []
+  end
+
+  def match(match_array)
+    word_sorted = @word.sort
+    match_array.each do |entry|
+      if word_sorted == entry.sort
+        return @word
+      end
+    end
+  end
+
+end
